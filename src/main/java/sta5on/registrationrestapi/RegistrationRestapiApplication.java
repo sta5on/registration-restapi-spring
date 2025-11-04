8f43948d1a460caae2e915c067856e03be274756
sta5on <stan.voronet@gmail.com>
2025-11-04 16:17:22 +0200

    sss

---
+++ src/main/java/sta5on/registrationrestapi/RegistrationRestapiApplication.java
@@ -0,0 +1,13 @@
+package sta5on.registrationrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistrationRestapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RegistrationRestapiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/sta5on/registrationrestapi/RegistrationRestapiApplicationTests.java
@@ -0,0 +1,13 @@
+package sta5on.registrationrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistrationRestapiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
